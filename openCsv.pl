63b0484c193ac2ea91537038557c1885dd4503cd
Taylor, Benjamin A <taylorba@ornl.gov>
2017-06-01 05:23:57 -0400

    openCsv.pl: read & display header

--- openCsv.pl
+++ openCsv.pl
@@ -7,6 +7,12 @@ sub main
     my $filename = "test.csv";
    
     open(INPUT, $filename) or die "Cannot open $filename";
+    
+    # Read the header line.
+    my $line = <INPUT>;
+
+    # Display the header, just to check things are working.
+    print($line);
 
     close(INPUT);
 }
