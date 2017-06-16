bbcb50ee3f1408cf8a682af703367c2644626773
Taylor, Benjamin A <taylorba@ornl.gov>
2017-06-16 09:33:07 -0400

    Removing 'add items' from script

--- accessArrayElements.pl
+++ accessArrayElements.pl
@@ -10,14 +10,3 @@ print "$items[0]\n";
 print "$items[1]\n";
 print "$items[2]\n";
 print "$items[$#items]\n";
-
-my @fruit = ('apple', 'orange', 'banana');
-
-# Add an item to the end of the array.
-
-push @fruit, 'pear';
-print Dumper(@fruit);
-
-unshift @fruit, 'grape';
-
-print Dumper(@fruit);
