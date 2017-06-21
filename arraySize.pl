d378b6dcb88e412507d03d0f2356b14eced87c44
Taylor, Benjamin A <taylorba@ornl.gov>
2017-06-21 19:11:43 -0400

    edits for arraySize.pl

--- arraySize.pl
+++ arraySize.pl
@@ -8,9 +8,9 @@ my @items = ("apple", "orange", "banana");
 my $size = @items;
 print "Array contains $size items.\n";
 
-my @items2 = ("apple", "orange", "banana");
+my @items2 = ("apple", "orange", "banana", "kiwi");
 
 # Below is same, but ues the scalar keyword 
 # to make things more explicit.
 my $size2 = scalar(@items2);
-print "Array contains $size items.\n"
+print "Array contains $size2 items.\n";
