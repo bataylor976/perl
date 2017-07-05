0956725e00c4539dc6801d23ffd68ff358425dfc
Taylor, Benjamin A <taylorba@ornl.gov>
2017-07-05 06:52:26 -0400

    updating removeFromArrays.pl

--- removeFromArrays.pl
+++ removeFromArrays.pl
@@ -3,15 +3,21 @@ use warnings;
 use Data::Dumper;
 
 my @fruit = ('apple', 'orange', 'banana');
+print "array before pop and shift: \n";
+print Dumper(@fruit);
+print"\n";
 
 # Remove the last item ('banana') and save it.
-my $end = pop @fruit;
-
+my $end = pop@fruit;
+print "pop'd item: \n";
 print "$end\n";
-# Remove first item off the array and save it.
+print"\n";
+
 # Remove first item off the array and save it.
 my $start = shift @fruit;
-
+print "shift'd item: \n";
 print "$start\n";
+print"\n";
 
+print "array remaining after pop and shift: \n";
 print Dumper(@fruit);
