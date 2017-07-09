8047c07c3d71321319b1604810e189dc411fb752
Taylor, Benjamin Allen <bataylor976@gmail.com>
2017-07-09 09:19:34 -0400

    updating sortArraysCustSortAlg.pl

--- sortArraysCustSortAlg.pl
+++ sortArraysCustSortAlg.pl
@@ -1,15 +1,18 @@
 use strict;
 use warnings;
+use Data::Dumper;
 
 $|=1;
 
 sub main
 {
     # Declare and initialize an array.
-    my @items = (0, 5, 10, 20);
-    
+    my @items = (10, 5, 20, 0);
+    print "Array dump before sort: \n" . Dumper(@items) . "\n";
+
     # Sort the array.
     @items = sort {$a <=> $b} @items;
+    print "Array dump after sort: \n" . Dumper(@items) . "\n";
 
     # print the array contents.
     foreach my $item(@items) {
